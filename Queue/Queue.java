1d75f1677b2abdd7ac06d50e1b46504d5f0ef2a9
chirag bhalotia <me@chirag.codes>
2022-09-18 14:12:22 +0530

    changed a lot

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -9,6 +9,7 @@ public class Queue {
     }
 
     void addEle(int var_num) {
+        // checking if its possible to add elements
         if (var_rear < var_size) {
             var_arr[++var_rear] = var_num;
             return;
@@ -17,6 +18,7 @@ public class Queue {
     }
 
     int delEle() {
+        // checking if its possible to delete elements
         if (var_rear == var_front) {
             System.out.println("Underflow");
             var_front = 0;
@@ -27,6 +29,7 @@ public class Queue {
     }
 
     void display() {
+        // checking if the queue is empty
         if (var_front == var_rear) {
             System.out.println("Empty");
             return;
@@ -34,6 +37,36 @@ public class Queue {
         for (int i = var_front + 1; i <= var_rear; i++) {
             System.out.println(var_arr[i]);
         }
+    }
+
+    public static void main(String[] args) {
+        Queue ob = new Queue(4);
+        // Adding elements
+        System.out.println("Adding Elements");
+        ob.addEle(3);
+        ob.addEle(3);
+        ob.addEle(3);
+        ob.addEle(3);
+        // Adding extra element to show case of overflow
+        ob.addEle(3);
+        // Displaying elements after adding elements
+        System.out.println("Elements: ");
+        ob.display();
+        // Removing elements
+        System.out.println("Removing Elements");
+        ob.delEle();
+        ob.delEle();
+        // Displaying elements after removing
+        System.out.println("Elements after removing 2 elements: ");
+        ob.display();
+        System.out.println("Removing the rest of the elements");
+        ob.delEle();
+        ob.delEle();
+        // Removing extra element to show case of underflow
+        ob.delEle();
+        // displaying empty queue
+        ob.display();
+
     }
 
 }
