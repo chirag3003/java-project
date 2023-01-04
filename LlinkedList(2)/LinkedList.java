f7289a6130a9a4c16f9826fa74b865252832cde0
Chirag Bhalotia <me@chirag.codes>
2023-01-04 20:47:00 +0530

    updated linkedlist

--- LlinkedList(2)/LinkedList.java
+++ LlinkedList(2)/LinkedList.java
@@ -3,9 +3,10 @@ class Node {
     Node next;
 }
 
-class LinkedList2 {
+class LinkedList {
     Node head;
-    //creates a new node at the end of the list
+
+    // creates a new node at the end of the list
     public void insert(int data) {
         if (head == null) {
             head = new Node();
@@ -22,7 +23,8 @@ class LinkedList2 {
             current.next = newNode;
         }
     }
-    //deletes the last node
+
+    // deletes the last node
     public void delete() {
         if (head == null)
             return;
@@ -37,9 +39,9 @@ class LinkedList2 {
         current.next = null;
     }
 
-    //joins two list together
-    public void joinNode(LinkedList2 list) {
-        if (head == null){
+    // joins two list together
+    public void joinList(LinkedList list) {
+        if (head == null) {
             head = list.head;
             return;
         }
@@ -59,43 +61,44 @@ class LinkedList2 {
     }
 
     public static void main(String[] args) {
-        LinkedList2 list1 = new LinkedList2();
-        //inserting elements in the first List
+        LinkedList list1 = new LinkedList();
+        // inserting elements in the first List
         list1.insert(1);
         list1.insert(2);
         list1.insert(3);
         list1.insert(4);
-        //display first list
+        // display first list
         System.out.println("First List: ");
         list1.printList();
-        
-        LinkedList2 list2 = new LinkedList2();
-        //inserting elements in the second List
+
+        LinkedList list2 = new LinkedList();
+        // inserting elements in the second List
         list2.insert(5);
         list2.insert(6);
         list2.insert(7);
-        //display second list
+        // display second list
         System.out.println("Second List: ");
         list2.printList();
-        
-        //joining two lists
-        list1.joinNode(list2);
+
+        // joining two lists
+        list1.joinList(list2);
         System.out.println("Joined List: ");
         list1.printList();
     }
 }
 
 /*
-    1. Define a Node class with fields for data and a reference to the next node.
-    2. Define a LinkedList class with methods for inserting a node, deleting a node, joining two lists and  printing the list.
-    3. In the main method:
-            Create a new LinkedList object.
-            Use the insert method to add some values to the list.
-            Use the printList method to print the list.
-            Create a second list
-            Use the insert method to insert some values to the second list.
-            Use the printList method to print the second list.
-            Use the joinList method to join the second list in the first.
-            Use the printList method to print the updated first list.
-    4. End the program.
+ * 1. Define a Node class with fields for data and a reference to the next node.
+ * 2. Define a LinkedList class with methods for inserting a node, deleting a
+ * node, joining two lists and printing the list.
+ * 3. In the main method:
+ * Create a new LinkedList object.
+ * Use the insert method to add some values to the list.
+ * Use the printList method to print the list.
+ * Create a second list
+ * Use the insert method to insert some values to the second list.
+ * Use the printList method to print the second list.
+ * Use the joinList method to join the second list in the first.
+ * Use the printList method to print the updated first list.
+ * 4. End the program.
  */
