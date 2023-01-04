b5bf85d43891ad63964b694ac26fbc3f4a911703
Chirag Bhalotia <me@chirag.codes>
2023-01-04 20:43:24 +0530

    updated

--- LlinkedList(2)/LinkedList2.java
+++ LlinkedList(2)/LinkedList2.java
@@ -5,16 +5,8 @@ class Node {
 
 class LinkedList2 {
     Node head;
-    //creates a new node at the beginning of the list
-    public void insertAtBeginning(int data) {
-        Node newNode = new Node();
-        newNode.data = data;
-        newNode.next = head;
-        head = newNode;
-    }
-
     //creates a new node at the end of the list
-    public void insertAtEnd(int data) {
+    public void insert(int data) {
         if (head == null) {
             head = new Node();
             head.data = data;
@@ -30,23 +22,8 @@ class LinkedList2 {
             current.next = newNode;
         }
     }
-    //creates a new note after the given node
-    public void insertAfter(Node previous, int data) {
-        if (previous == null)
-            return;
-        Node newNode = new Node();
-        newNode.data = data;
-        newNode.next = previous.next;
-        previous.next = newNode;
-    }
-    //deletes the first node
-    public void deleteFirst() {
-        if (head == null)
-            return;
-        head = head.next;
-    }
     //deletes the last node
-    public void deleteLast() {
+    public void delete() {
         if (head == null)
             return;
         if (head.next == null) {
@@ -60,22 +37,17 @@ class LinkedList2 {
         current.next = null;
     }
 
-    //finds the node with the given data and deletes it
-    public void deleteAny(int data) {
-        if (head == null)
-            return;
-        if (head.data == data) {
-            head = head.next;
+    //joins two list together
+    public void joinNode(LinkedList2 list) {
+        if (head == null){
+            head = list.head;
             return;
         }
         Node current = head;
         while (current.next != null) {
-            if (current.next.data == data) {
-                current.next = current.next.next;
-                return;
-            }
             current = current.next;
         }
+        current.next = list.head;
     }
 
     public void printList() {
@@ -87,53 +59,43 @@ class LinkedList2 {
     }
 
     public static void main(String[] args) {
-        LinkedList2 list = new LinkedList2();
-        // Insert some values at the beginning of the list
-        list.insertAtBeginning(4);
-        list.insertAtBeginning(3);
-        list.insertAtBeginning(2);
-        list.insertAtBeginning(1);
-        // Print the list
-        list.printList();
-        // Insert a new value at the end of the list
-        list.insertAtEnd(5);
-        // Print the updated list
-        list.printList();
-        // Insert a new value after the second node
-        list.insertAfter(list.head.next, 6);
-        // Print the updated list
-        list.printList();
-        // Delete the first node
-        list.deleteFirst();
-        // Print the updated list
-        list.printList();
-        // Delete the last node
-        list.deleteLast();
-        // Print the updated list
-        list.printList();
-        // Delete a node with a given value
-        list.deleteAny(3);
-        // Print the updated list
-        list.printList();
+        LinkedList2 list1 = new LinkedList2();
+        //inserting elements in the first List
+        list1.insert(1);
+        list1.insert(2);
+        list1.insert(3);
+        list1.insert(4);
+        //display first list
+        System.out.println("First List: ");
+        list1.printList();
+        
+        LinkedList2 list2 = new LinkedList2();
+        //inserting elements in the second List
+        list2.insert(5);
+        list2.insert(6);
+        list2.insert(7);
+        //display second list
+        System.out.println("Second List: ");
+        list2.printList();
+        
+        //joining two lists
+        list1.joinNode(list2);
+        System.out.println("Joined List: ");
+        list1.printList();
     }
 }
 
 /*
     1. Define a Node class with fields for data and a reference to the next node.
-    2. Define a LinkedList class with methods for inserting at the beginning, end, and after a given node, deleting the first, last, and any node with a given value, and printing the list.
+    2. Define a LinkedList class with methods for inserting a node, deleting a node, joining two lists and  printing the list.
     3. In the main method:
             Create a new LinkedList object.
-            Use the insertAtBeginning method to add some values to the beginning of the list.
+            Use the insert method to add some values to the list.
             Use the printList method to print the list.
-            Use the insertAtEnd method to add a new value to the end of the list.
-            Use the printList method to print the updated list.
-            Use the insertAfter method to add a new value after a given node.
-            Use the printList method to print the updated list.
-            Use the deleteFirst method to delete the first node of the list.
-            Use the printList method to print the updated list.
-            Use the deleteLast method to delete the last node of the list.
-            Use the printList method to print the updated list.
-            Use the deleteAny method to delete a node with a given value.
-            Use the printList method to print the updated list.
+            Create a second list
+            Use the insert method to insert some values to the second list.
+            Use the printList method to print the second list.
+            Use the joinList method to join the second list in the first.
+            Use the printList method to print the updated first list.
     4. End the program.
  */
